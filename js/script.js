609bd80aa0f532890baab706c9f5c359990c38b2
Luan-Novais <luan.novaiss@outlook.com>
2020-10-25 18:41:05 -0300

    Editando responsividade

--- js/script.js
+++ js/script.js
@@ -37,3 +37,12 @@ function toggleMenu() {
 
 }
 
+function toggleMenu(){
+	var menu = document.getElementById('menu-mobile');
+
+	if(menu.style.display == 'none'){
+		menu.style.display = 'block';
+	}else{
+		menu.style.display = 'none';
+	}
+}
